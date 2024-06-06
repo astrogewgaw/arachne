10a2aa950e6a2b2d5b09e28d373fe483b4430c61
raghavwani <raghav.wani@gmail.com>
2024-06-06 16:31:58 +0530

    Improved Arachne: Boundary condition formula applied, dual peaks issue resolved

--- arachne.c
+++ arachne.c
@@ -170,22 +170,59 @@ double clip(double x, double x1, double x2)
 double prob(double x) { return 0.5 + 0.5 * erf(x / sqrt(2)); }
 
 /* Calculate probability of shifting the bit: 8 bit injection */
-int cal_bit_shift_prob(int in, double lvl, double signal)
-{
-  double plvl = 0, pval = 0;
-  int out = in;
-  for (int m = 0; m <= 255 - in; m++)
-  {
-    plvl = ((prob(min((in - 127) * lvl, (in + m - 127) * lvl - signal)) - prob(max((in - 128) * lvl, (in + m - 128) * lvl - signal))) /
-            (prob((in - 127) * lvl) - prob((in - 128) * lvl))); // prob n--> n+m
-    if (pval < plvl)
-    {
-      pval = plvl;
-      out = in + m;
+int cal_bit_shift_prob(int in, double lvl, double signal){
+     double plvl = 0, pval = 0;
+     int out = in; // prob 255--> 255 
+     if (in == 0){
+       for (int m = 0; m <= 255 - in; m++){
+         
+	if (in +m == 0){
+	   plvl = prob(-127*lvl - signal)/ prob(-127*lvl); //prob 0--> 0
+	   if (pval < plvl){
+		pval =plvl;
+		out = in+m;
+	   }
+	} else if (in+m==255){
+           plvl = (prob(-127 * lvl) - prob(127 * lvl - signal)) /
+                   prob( - 127 * lvl); // prob o--> 255
+          if (pval < plvl){
+            pval = plvl;
+            out = in + m;
+          } 
+        } 
+        else{
+          plvl = (prob(min(-127 * lvl, (m-127) * lvl - signal)) - prob((m-128) * lvl - signal)) /
+                  prob((in - 127) * lvl); // prob 0--> m
+          if (pval < plvl){
+            pval = plvl;
+            out = in + m;
+          } 
+        }
+      }
     }
+    else{
+      for (int m = 0; m <= 255 - in; m++){
+        if (in+m==255){
+          plvl = ((prob((in - 127) * lvl) - prob(max((in - 128) * lvl, 127 * lvl - signal))) /
+                (prob((in - 127) * lvl) - prob((in - 128) * lvl))); // prob n--> 255
+          if (pval < plvl){
+            pval = plvl;
+            out = in + m;
+          } 
+        } 
+        else{
+          plvl = ((prob(min((in - 127) * lvl, (in + m - 127) * lvl - signal)) - prob(max((in - 128) *lvl, (in + m - 128) * lvl - signal))) / 
+                  (prob((in - 127) * lvl) - prob((in - 128) * lvl))); // prob n--> n+m
+          if (pval < plvl)
+          {
+            pval = plvl;
+            out = in + m;
+          } 
+        } 
+      } 
+    } 
+    return out;
   }
-  return out;
-}
 
 /* Print Arachne's logo. */
 void print_logo()
@@ -445,7 +482,7 @@ int main(int argc, char *argv[])
   long M, N, nnz = 0, blkbeg, blkend, I, break_point_index = 0;
   double dm, flux, width, tburst, offset, blktime, lvl = 0.030765, signal;
   int blknt = BLKSIZE / 4096;
-  double sigma = cfg.tsys / cfg.sysgain / sqrt(2 * cfg.dt * (cfg.df * 1e6)); /* Ideal RMS calculation. */
+  double sigma = cfg.tsys /( cfg.sysgain * sqrt(2 * cfg.dt * (cfg.df * 1e6))); /* Ideal RMS calculation. */
 
   while (keep)
   {
@@ -534,7 +571,7 @@ int main(int argc, char *argv[])
             }
             I = I % (long)BLKSIZE;
             in = raw[I];
-            signal = fluxes[i] / sigma;
+            signal = fluxes[i] /( sigma);
             out = cal_bit_shift_prob(in, lvl, signal);
             raw[I] = out;
           }
@@ -556,7 +593,7 @@ int main(int argc, char *argv[])
             }
             I = I % (long)BLKSIZE;
             in = raw[I];
-            signal = fluxes[i] / sigma;
+            signal = fluxes[i] / (sigma);
             out = cal_bit_shift_prob(in, lvl, signal);
             raw[I] = out;
           }
